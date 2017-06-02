634df4fdb1c9a54111ed9443933ff937d0b5c8ca
BoySanic <boysonicrevived@gmail.com>
2017-06-02 10:14:47 -0400

    Forgot a semilcolon
    Christ, 2 mistakes in one line dude

--- include/rest.h
+++ include/rest.h
@@ -1,2 +1,2 @@
 
-const char *DISCORD_REST_BASE_URL = "https://discordapp.com/api" //this should never change unless discord does.
+const char *DISCORD_REST_BASE_URL = "https://discordapp.com/api"; //this should never change unless discord does.
